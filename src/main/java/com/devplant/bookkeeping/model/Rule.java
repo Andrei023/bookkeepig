e904e6c13c3d7fe439eac375c9958b2a4c8f8856
Andrei Panoiu <andrei.panoiu@novidea.com>
2023-06-01 01:05:40 +0300

    added no args constructor

--- src/main/java/com/devplant/bookkeeping/model/Rule.java
+++ src/main/java/com/devplant/bookkeeping/model/Rule.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Rule {
 
